ec246f8f33749b149e1ffbe83ca7ad1c37ce264f
Jacek Gębal <jgebal@gmail.com>
2016-08-21 23:05:05 +0100

    Trying to combine projects

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 
 [ -n "$ORACLE_COOKIE" ] || { echo "Missing ORACLE_COOKIE environment variable!"; exit 1; }
-[ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
 
 cd "$(dirname "$(readlink -f "$0")")"
 npm install bluebird node-phantom-simple
@@ -12,9 +11,9 @@ export USER_AGENT='Mozilla/5.0'
 echo > "$COOKIES"
 chmod 600 "$COOKIES"
 
-exec node download.js oracle11g/xe/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 exec node download.js oracle12c/121020/linuxamd64_12102_database_1of2.zip
 exec node download.js oracle12c/121020/linuxamd64_12102_database_2of2.zip
+exec node download.js oracle11g/xe/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 
 mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip $TRAVIS_BUILD_DIR/dockerfiles/11.2.0.2
 mv linuxamd64_12102_database_1of2.zip  $TRAVIS_BUILD_DIR/dockerfiles/12.1.0.2
